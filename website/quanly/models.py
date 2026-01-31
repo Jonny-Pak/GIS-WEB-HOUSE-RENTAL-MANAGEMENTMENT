0101ed1a0193953826c89bb2027a329d96bc533a
Ngọc Thuận <thuanln1907@gmail.com>
2026-01-31 22:57:26 +0700

    Update models.py in folder quanly

--- website/quanly/models.py
+++ website/quanly/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -12,37 +13,41 @@ class Furniture(models.Model):
 
 # Bảng Nhà
 class House(models.Model):
-    name = models.CharField(max_length=200, verbose_name="Tên Căn Nhà / Tòa nhà")
+
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="Chủ nhà", null=True, blank=True)
+    owner_phone = models.CharField(max_length=10, verbose_name="Số điện thoại")
+    name = models.CharField(max_length=200, verbose_name="Tên Căn Nhà")
     address = models.TextField(verbose_name="Địa chỉ nhà")
-    
+    price = models.IntegerField(verbose_name="Giá cho thuê (VNĐ)")
+    deposit = models.IntegerField(verbose_name="Tiền cọc (VNĐ)")
+    area = models.IntegerField(verbose_name="Diện tích nhà (m2)", default=20)
+    max_people = models.IntegerField(verbose_name="Số người ở tối đa", default=4)
+    current_people = models.IntegerField(verbose_name="Số người đang ở", default=0)
     lat = models.FloatField(verbose_name="Vĩ độ", default=0.0)
     long = models.FloatField(verbose_name="Kinh độ", default=0.0)
-    
     description = models.TextField(blank=True, verbose_name="Mô tả căn nhà")
+    is_rented = models.BooleanField(default=False, verbose_name="Trạng thái thuê?")
+    is_approved = models.BooleanField(default=False, verbose_name="Trạng thái duyệt bài?")
+
     
     def __str__(self):
-        return self.name
+        return f"{self.name} - {self.price: ,} VNĐ"
 
 # Bảng Phòng
 class Room(models.Model):
     house = models.ForeignKey(House, on_delete=models.CASCADE, verbose_name="Thuộc căn nhà")
-    
     room_number = models.CharField(max_length=50, verbose_name="Số phòng")
-    area = models.IntegerField(verbose_name="Diện tích (m2)", default=20)
-    price = models.IntegerField(verbose_name="Giá cho thuê (VNĐ)")
-    deposit = models.IntegerField(verbose_name="Tiền cọc (VNĐ)")
-    max_people = models.IntegerField(verbose_name="Số người ở tối đa", default=4)
-    current_people = models.IntegerField(verbose_name="Số người đang ở", default=0)
+    room_name = models.CharField(max_length=100, verbose_name="Tên phòng" )
+    area = models.IntegerField(verbose_name="Diện tích phòng (m2)", default=20)
     furnitures = models.ManyToManyField(Furniture, verbose_name="Nội thất đi kèm", blank=True)
-    is_rented = models.BooleanField(default=False, verbose_name="Trạng thái thuê?")
 
     def __str__(self):
-        return f"{self.house.name} - {self.room_number}"
+        return f"{self.room_number} - {self.house.name}"
 
 # Bảng Hình ảnh
 class RoomImage(models.Model):
     room = models.ForeignKey(Room, on_delete=models.CASCADE, related_name='images')
-    image = models.ImageField(upload_to='room_images/', verbose_name="Hình ảnh phòng")
+    image = models.ImageField(upload_to='room_images/', verbose_name="Hình ảnh phòng/nhà")
 
     def __str__(self):
         return f"Ảnh của {self.room.room_number}"
@@ -52,11 +57,12 @@ class Contract(models.Model):
     house = models.OneToOneField(House, on_delete=models.CASCADE, verbose_name="Hợp đồng thuê cho nhà")
     
     tenant_name = models.CharField(max_length=100, verbose_name="Họ tên khách thuê")
-    tenant_phone = models.CharField(max_length=15, verbose_name="Số điện thoại")
+    tenant_phone = models.CharField(max_length=10, verbose_name="Số điện thoại")
     tenant_id = models.CharField(max_length=20, verbose_name="CCCD")
     start_date = models.DateField(verbose_name="Ngày bắt đầu thuê")
     end_date = models.DateField(verbose_name="Ngày kết thúc thuê")
     total_value = models.IntegerField(verbose_name="Tổng giá trị hợp đồng (VNĐ)", null=True, blank=True)
 
-def __str__(self):
+    def __str__(self):
         return f"Hợp đồng: {self.tenant_name} thuê {self.house.name}"
+
