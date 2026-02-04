85478d4f789551888f1a001d2611c7bdbfbd7895
Ngọc Thuận <thuanln1907@gmail.com>
2026-02-04 13:54:02 +0700

    Update View/Urls/Static/Templates
    Cập nhật:
    - Thêm View cho trang home
    - Thêm Urls cho trang home
    - Thêm folder static bao gồm css/images/font
    - Thêm hmtl cho trang home.html và tạo sườn cho trang web base.html

--- website/quanly/views.py
+++ website/quanly/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home(request):
+    return render(request, 'quanly/home.html')
+
+def register(request):
+    return render(request, 'quanly/register.html')
