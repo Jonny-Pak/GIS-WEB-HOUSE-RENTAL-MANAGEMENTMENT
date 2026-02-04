6463b05408bb59f9b4def9d87b2c66b8c5ed0298
Duong Minh Triet <1250080211@sv.hcmunre.edu.vn>
2026-02-04 19:07:31 +0700

    Giao diện đăng nhập, đăng ký

--- website/quanly/urls.py
+++ website/quanly/urls.py
@@ -4,4 +4,8 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('login/', auth_views.LoginView.as_view(template_name='quanly/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='/'), name='logout'),
+    path('register/', views.register, name='register'),
+    path('contact/', views.contact, name='contact'),
 ]
--- website/quanly/views.py
+++ website/quanly/views.py
@@ -4,5 +4,12 @@ from django.shortcuts import render
 def home(request):
     return render(request, 'quanly/home.html')
 
+def login_view(request):
+    return render(request, 'quanly/login.html')
+
 def register(request):
     return render(request, 'quanly/register.html')
+
+def contact(request):
+    return render(request, 'quanly/contact.html')
+
