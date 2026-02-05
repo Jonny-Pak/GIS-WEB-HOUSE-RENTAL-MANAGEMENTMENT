23db899919cbf36d59c0a8f5f89ba50d885681f9
Duong Minh Triet <1250080211@sv.hcmunre.edu.vn>
2026-02-05 22:35:57 +0700

    Updated login, regisster, dashboard, houses

--- website/quanly/urls.py
+++ website/quanly/urls.py
@@ -4,8 +4,11 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('login/', auth_views.LoginView.as_view(template_name='quanly/login.html'), name='login'),
+    path('login/', auth_views.LoginView.as_view(template_name='quanly/accounts/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='/'), name='logout'),
     path('register/', views.register, name='register'),
-    path('contact/', views.contact, name='contact'),
+    path('detail/', views.house_detail, name='house_detail'),
+    path('dashboard/', views.dashboard, name='dashboard'),
+    path('post-house/', views.post_house, name='post_house'),
+    path('manage_post/', views.manage_post, name='manage_post')
 ]
