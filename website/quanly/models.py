4f4557d5f4af067a7654b659e49220eaaeaac684
Ngọc Thuận <thuanln1907@gmail.com>
2026-02-05 22:50:06 +0700

    Updated models

--- website/quanly/models.py
+++ website/quanly/models.py
@@ -14,30 +14,68 @@ class Furniture(models.Model):
 # Bảng Nhà
 class House(models.Model):
 
+    HOUSE_TYPE_CHOICES = [
+        ('nguyencan', 'Nhà nguyên căn'),
+        ('chungcu', 'Căn hộ chung cư'),
+        ('phong_tro', 'Phòng Trọ'),
+    ]
+
+    STATUS_CHOISE = [
+        ('pending', 'Chờ duyệt'),
+        ('approved', 'Đã duyệt'),
+        ('hidden', 'Đã ẩn'),
+        ('rejected', 'Bị từ chối'),
+    ]
+
+    DISTRICT_CHOPCES = [
+        ('q1', 'Quận 1'),
+        ('q2', 'Quận 2'),
+        ('q3', 'Quận 3'),
+        ('q4', 'Quận 4'),
+        ('q5', 'Quận 5'),
+        ('q6', 'Quận 6'),
+        ('q7', 'Quận 7'),
+        ('q8', 'Quận 8'),
+        ('q9', 'Quận 9'),
+        ('q10', 'Quận 10'),
+        ('qtb', 'Quận Tân Bình'),
+        ('qtp', 'Quận Tân Phú'),
+        ('qbt', 'Quận Bình Thạnh'),
+        ('td', 'Thủ Đức'),
+    ]
+
     owner = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="Chủ nhà", null=True, blank=True)
     owner_phone = models.CharField(max_length=10, verbose_name="Số điện thoại")
+
     name = models.CharField(max_length=200, verbose_name="Tên Căn Nhà")
-    address = models.TextField(verbose_name="Địa chỉ nhà")
+    house_type = models.CharField(max_length=20, choices=HOUSE_TYPE_CHOICES, default='phong_tro', verbose_name="Loại nhà")
     price = models.IntegerField(verbose_name="Giá cho thuê (VNĐ)")
     deposit = models.IntegerField(verbose_name="Tiền cọc (VNĐ)")
     area = models.IntegerField(verbose_name="Diện tích nhà (m2)", default=20)
-    max_people = models.IntegerField(verbose_name="Số người ở tối đa", default=4)
-    current_people = models.IntegerField(verbose_name="Số người đang ở", default=0)
+
+    district = models.CharField(max_length=10, choices= DISTRICT_CHOPCES, verbose_name= "Chọn Quận")
+    address = models.TextField(verbose_name="Địa chỉ nhà")
     lat = models.FloatField(verbose_name="Vĩ độ", default=0.0)
     long = models.FloatField(verbose_name="Kinh độ", default=0.0)
+
+    max_people = models.IntegerField(verbose_name="Số người ở tối đa", default=4)
+    current_people = models.IntegerField(verbose_name="Số người đang ở", default=0)
     description = models.TextField(blank=True, verbose_name="Mô tả căn nhà")
-    is_rented = models.BooleanField(default=False, verbose_name="Trạng thái thuê?")
-    is_approved = models.BooleanField(default=False, verbose_name="Trạng thái duyệt bài?")
+
+    main_image = models.ImageField(upload_to= 'house/', null = True, blank= True, verbose_name= "Ảnh đại diện phòng")
+    status = models.CharField(max_length=20, choices= STATUS_CHOISE, verbose_name= "Trạng thái")
+    
+    create_at = models.DateTimeField(auto_now_add=True, verbose_name="Ngày đăng")
+    update_at = models.DateTimeField(auto_now=True, verbose_name="Cập nhật lần cuối")
 
     
     def __str__(self):
-        return f"{self.name} - {self.price: ,} VNĐ"
+        return f"[{self.get_status_display()}] {self.name}"
 
 # Bảng Phòng
 class Room(models.Model):
     house = models.ForeignKey(House, on_delete=models.CASCADE, verbose_name="Thuộc căn nhà")
     room_number = models.CharField(max_length=50, verbose_name="Số phòng")
-    room_name = models.CharField(max_length=100, verbose_name="Tên phòng" )
     area = models.IntegerField(verbose_name="Diện tích phòng (m2)", default=20)
     furnitures = models.ManyToManyField(Furniture, verbose_name="Nội thất đi kèm", blank=True)
 
@@ -45,12 +83,12 @@ class Room(models.Model):
         return f"{self.room_number} - {self.house.name}"
 
 # Bảng Hình ảnh
-class RoomImage(models.Model):
-    room = models.ForeignKey(Room, on_delete=models.CASCADE, related_name='images')
-    image = models.ImageField(upload_to='room_images/', verbose_name="Hình ảnh phòng/nhà")
+class HouseImage(models.Model):
+    house = models.ForeignKey(House, on_delete=models.CASCADE, related_name='images')
+    image = models.ImageField(upload_to='house_images/', verbose_name="Hình ảnh")
 
     def __str__(self):
-        return f"Ảnh của {self.room.room_number}"
+        return f"Ảnh của {self.house.name}"
 
 # Bảng Hợp đồng Thuê Nhà
 class Contract(models.Model):
