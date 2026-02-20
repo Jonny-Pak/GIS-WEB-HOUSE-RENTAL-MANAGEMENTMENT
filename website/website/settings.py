3e1c3a4d6616b281446a13f946bd0dd520c2ac7f
tinhvippro123 <lethanhtinh2004@gmail.com>
2026-02-20 22:58:19 +0700

    chore: configure MEDIA_ROOT in settings

--- website/website/settings.py
+++ website/website/settings.py
@@ -120,3 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Cấu hình đường dẫn chứa file upload
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR.parent, 'media')
